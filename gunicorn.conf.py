c8c60336296784a768b5905f4d0af525df32b348
romanoa77 <37339804+romanoa77@users.noreply.github.com>
2024-04-18 22:58:17 +0200

    Update and rename example_config.py to gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,31 @@
-# Sample Gunicorn configuration file.
+# Gunicorn configuration file.
+#This is a modified version of the example configuration file found in the official gunicorn repository https://github.com/benoitc/gunicorn/tree/master
+
+"""
+2009-2024 (c) Benoît Chesneau <benoitc@gunicorn.org>
+2009-2015 (c) Paul J. Davis <paul.joseph.davis@gmail.com>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 #
 # Server socket
@@ -18,7 +45,7 @@
 #       range.
 #
 
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:8080'
 backlog = 2048
 
 #
@@ -65,7 +92,7 @@ backlog = 2048
 #       A positive integer. Generally set in the 1-5 seconds range.
 #
 
-workers = 1
+workers = 2
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 30
